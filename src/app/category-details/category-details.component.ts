0a4fe1fd8fda0ab601a6528f902299728256fa4b
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2021-02-13 02:03:24 +0300

    feat(category-details): edit and delete support

--- src/app/category-details/category-details.component.ts
+++ src/app/category-details/category-details.component.ts
@@ -4,6 +4,10 @@ import { CategoryTree } from '../navigation/state/interface';
 import { CategoryDetailsFacade } from './state/category-details.facade';
 import { CategoryDetails } from './state/interface';
 import {SubscriptionListener} from "../core/subscription-listener";
+import { Record } from '../detail/state/interface';
+import { FormModeEnum } from '../shared/components/create-dialog/form-mode.enum';
+import { DetailsFacade } from '../detail/state/details.facade';
+import { DialogManagerService } from '../shared/services/dialog-manager.service';
 
 @Component({
   selector: 'app-category-details',
@@ -16,8 +20,10 @@ export class CategoryDetailsComponent extends SubscriptionListener implements On
   public categoryDetails: CategoryDetails;
 
   constructor(
-    private categoryDetailsFacade: CategoryDetailsFacade,
     private route: ActivatedRoute,
+    private detailsFacade: DetailsFacade,
+    private dialogManagerService: DialogManagerService,
+    private categoryDetailsFacade: CategoryDetailsFacade,
   ) {
     super();
   }
@@ -28,10 +34,41 @@ export class CategoryDetailsComponent extends SubscriptionListener implements On
         this.categoryDetails = curr.value;
       });
 
+    this.listenLoadedEntity$([
+      this.detailsFacade.editDetail$,
+      this.detailsFacade.deleteDetail$,
+    ]).subscribe(() => {
+      this.categoryDetailsFacade.loadCategoryDetails(this.id);
+    });
+
     this.route.params.subscribe(params => {
       this.id = params['id'];
       this.categoryDetailsFacade.loadCategoryDetails(this.id);
     });
   }
 
+  public onEdit(id: string): void {
+    const data = {
+      mode: FormModeEnum.Edit,
+      parent: this.id,
+      formData: this.findRecord(id),
+    };
+
+    this.dialogManagerService
+      .openRecordDialog(data)
+      .subscribe((record: Record) => {
+        this.detailsFacade.editRecord(record);
+      });
+  }
+
+  public onDelete(id: string): void {
+    this.detailsFacade.deleteRecord(id);
+  }
+
+  private findRecord(id: string): Record {
+    return this.categoryDetails.tables
+      .map((table) => table.data)
+      .reduce((acc, curr) => acc.concat(...curr))
+      .find((record) => record.id === id);
+  }
 }
