b5784347655fc781ffc9f3dcc62390d0e1ee666c
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2021-02-19 00:14:49 +0300

    feat(categories-tree): add icon support

--- src/app/navigation/state/interface.ts
+++ src/app/navigation/state/interface.ts
@@ -2,11 +2,13 @@ export interface Category {
   id: string;
   name: string;
   parent: string;
+  illustration?: string;
 }
 
 export interface CategoryTree {
   id: string;
   name: string;
   parent?: string;
+  illustration? : string;
   children?: Array<CategoryTree>;
 }
