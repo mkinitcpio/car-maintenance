720db2e95a393c3356df1b4f6af199b7ff0a8331
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2021-02-23 13:48:45 +0300

    refactor(database-select): translation

--- src/app/database-select/database-select.module.ts
+++ src/app/database-select/database-select.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatabaseSelectComponent } from './database-select.component';
 import {MatButtonModule} from "@angular/material/button";
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [DatabaseSelectComponent],
   imports: [
     CommonModule,
     MatButtonModule,
+    TranslateModule,
   ],
   exports: [
     DatabaseSelectComponent,
