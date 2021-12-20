3f7232c3f1c78cf35afa858c88f33eaac2f2cf50
Aliaksandr Kazhamiaka <aliaksandr_kazhamiaka@epam.com>
2021-12-20 19:02:40 +0300

    feat(print-dialog): print template

--- src/app/shared/components/print-dialog/preview-page/preview-page.component.ts
+++ src/app/shared/components/print-dialog/preview-page/preview-page.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 import { ColumnConfig, PreviewPageConfig } from './preview-page-config';
 import { Record } from '../../../../detail/state/interface';
+import { ElectronService } from '../../../../core/services';
 
 @Component({
   selector: 'app-preview-page',
   templateUrl: './preview-page.component.html',
   styleUrls: ['./preview-page.component.scss'],
 })
-export class PreviewPageComponent {
+export class PreviewPageComponent implements AfterViewInit {
 
   @Input()
   config: PreviewPageConfig;
@@ -16,9 +17,65 @@ export class PreviewPageComponent {
   @Input()
   records: Array<Record>;
 
+  @ViewChild('print', { read: ElementRef }) print;
+
   public readonly dateFormat = 'd MMM, y';
 
-  constructor() { }
+  constructor(    private electronService: ElectronService,) { }
+
+  ngAfterViewInit(): void {
+    console.log(this.print.nativeElement.outerHTML);
+    const options = {
+      silent: false,
+      printBackground: false,
+      color: false,
+      margin: {
+        marginType: 'printableArea'
+      },
+      landscape: false,
+      pagesPerSheet: 1,
+      collate: false,
+      copies: 1,
+      header: 'Header of the Page',
+      footer: 'Footer of the Page'
+    };
+
+    const win = new this.electronService.remote.BrowserWindow({
+      show: false,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+    win.loadURL(`data:text/html;charset=utf-8,<head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>MyYTitle</title> <style type="text/css">
+    @media print {
+      table {
+        border: 1px solid gray;
+        border-collapse: collapse;
+      }
+
+      th, td, th {
+        font-family: Ubuntu;
+        border: 1px solid black;
+        padding: 4pt;
+        border-collapse: collapse;
+      }
+
+      td.mat-column-date,
+      td.mat-column-cost,
+      td.mat-column-mileage {
+        white-space: nowrap;
+      }
+
+      th {
+        text-align: left;
+      }
+    }
+    </style></head> <body>${this.print.nativeElement.outerHTML}</body>`);
+
+    win.webContents.on('did-finish-load', () => {
+      win.webContents.executeJavaScript('window.print()');
+    });
+  }
 
   public trackById(columnConfig: ColumnConfig): string {
     return columnConfig.id;
