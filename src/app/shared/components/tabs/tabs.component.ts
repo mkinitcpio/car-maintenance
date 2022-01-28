7362d47b201bc00a32c1747b76d37e1a88b6cfd0
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-01-28 23:36:18 +0300

    refactor(tabs): style

--- src/app/shared/components/tabs/tabs.component.ts
+++ src/app/shared/components/tabs/tabs.component.ts
@@ -1,18 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('tabCoverRef', {static: true}) tabCoverRef: ElementRef;
+  @ViewChild('tabsRef', {static: true}) tabsRef: ElementRef;
+
+  @Input()
+  tabs: string[];
 
   public selectedtab = 0;
-  public tabWidth = 133.5;
+  public tabWidth = 0;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.setTabWidth(this.tabsRef, this.tabs.length);
+    this.tabCoverRef.nativeElement.style.width = `${this.tabWidth}px`;
+    this.updateTabCoverPosition(this.selectedtab);
+
+    const tabs = this.tabsRef.nativeElement.querySelectorAll('.tabs__tab');
+
+    tabs.forEach(tab => {
+      tab.style.width = `${this.tabWidth}px`;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  public onTabSelect(index: number): void {
+    this.selectedtab = index;
+    this.updateTabCoverPosition(index);
+  }
+
+  private setTabWidth(tabsRef: ElementRef, tabsCount: number): void {
+    this.tabWidth = tabsRef.nativeElement.clientWidth / tabsCount;
+  }
+
+  private updateTabCoverPosition(offset: number): void {
+    this.tabCoverRef.nativeElement.style.left = `${this.tabWidth * offset}px`;
+  }
 }
--- src/app/shared/components/tabs/tabs.module.ts
+++ src/app/shared/components/tabs/tabs.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs.component';
-
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
     TabsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
   ],
   exports: [
     TabsComponent,
