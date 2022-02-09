864c368eca5190876d0d6af20c617b0ee152fdc1
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-02-10 00:37:18 +0300

    refactor(empty): style

--- src/app/custom-icons-set.ts
+++ src/app/custom-icons-set.ts
@@ -1,11 +1,14 @@
 export const customIconsSet = [{
-    name: "add-rounded",
-    path: "assets/icons-rounded/add.svg",
-  },{
-    name: "menu-rounded",
-    path: "assets/icons-rounded/menu.svg",
-  },{
-    name: "github",
-    path: "assets/icons-rounded/github.svg",
-  },
+  name: "add-rounded",
+  path: "assets/icons-rounded/add.svg",
+}, {
+  name: "menu-rounded",
+  path: "assets/icons-rounded/menu.svg",
+}, {
+  name: "github",
+  path: "assets/icons-rounded/github.svg",
+}, {
+  name: "list-rounded",
+  path: "assets/icons-rounded/list.svg",
+},
 ];
