3b8076c897d2559dfba492aac8aa5243fde22fdd
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-03-07 01:17:19 +0300

    refactor(app): icons

--- src/app/custom-icons-set.ts
+++ src/app/custom-icons-set.ts
@@ -11,4 +11,27 @@ export const customIconsSet = [{
   name: "list-rounded",
   path: "assets/icons-rounded/list.svg",
 },
+{
+  name: "expand-more-rounded",
+  path: "assets/icons-rounded/expand-more.svg",
+},
+{
+  name: "tree-rounded",
+  path: "assets/icons-rounded/tree.svg",
+},{
+  name: "close-rounded",
+  path: "assets/icons-rounded/close.svg",
+},
+{
+  name: "delete-rounded",
+  path: "assets/icons-rounded/delete.svg",
+},
+{
+  name: "arrow-left-rounded",
+  path: "assets/icons-rounded/arrow-left.svg",
+},
+{
+  name: "export-pdf-rounded",
+  path: "assets/icons-rounded/export-pdf.svg",
+},
 ];
