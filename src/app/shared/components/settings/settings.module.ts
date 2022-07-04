1ffe7e946b5d404549d19b9601411ff931eda0e5
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-07-04 15:12:00 +0300

    (refactor:settings): styles

--- src/app/shared/components/settings/settings.module.ts
+++ src/app/shared/components/settings/settings.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { UbuntuCloseButtonModule } from '../../ubuntu/ubuntu-close-button/ubuntu-close-button.module';
 import { UbuntuContainerModule } from '../../ubuntu/ubuntu-container/ubuntu-container.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,7 +10,6 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { SettingsService } from './settings.service';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {ColorSelectorModule} from '../color-selector/color-selector.module';
@@ -28,7 +26,6 @@ import { ButtonModule } from '../button/button.module';
     MatButtonModule,
     MatFormFieldModule,
     MatSelectModule,
-    UbuntuCloseButtonModule,
     UbuntuContainerModule,
     TranslateModule,
     MatDividerModule,
