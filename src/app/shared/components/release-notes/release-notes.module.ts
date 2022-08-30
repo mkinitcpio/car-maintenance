711b54498f9ce717c3c60b8734cb3faae5488786
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-08-30 14:18:57 +0300

    fix(release-notes): remove unused import

--- src/app/shared/components/release-notes/release-notes.module.ts
+++ src/app/shared/components/release-notes/release-notes.module.ts
@@ -6,7 +6,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { UbuntuCloseButtonModule } from '../../ubuntu/ubuntu-close-button/ubuntu-close-button.module';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { UbuntuContainerModule } from '@shared/ubuntu/ubuntu-container/ubuntu-container.module';
@@ -25,7 +24,6 @@ import { UbuntuContainerModule } from '@shared/ubuntu/ubuntu-container/ubuntu-co
     MatSelectModule,
     MatFormFieldModule,
     TranslateModule,
-    UbuntuCloseButtonModule,
     MatDividerModule,
     UbuntuContainerModule,
   ]
