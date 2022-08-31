909b510a166b665ffad85f9db09c470d10bb9906
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-08-31 18:52:10 +0300

    fix(app): build

--- src/app/shared/components/rich-widget/rich-widget.component.ts
+++ src/app/shared/components/rich-widget/rich-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MetricSystemEnum } from '../settings/metric-system.enum';
 import { SettingsService } from '../settings/settings.service';
 
 import { DataType, Orientation } from './types';
@@ -13,9 +14,11 @@ export class RichWidgetComponent {
   @Input() tooltip: string;
   @Input() title: string;
   @Input() icon: string;
-  @Input() value: string;
+  @Input() value: number;
   @Input() type: DataType;
   @Input() orientation: Orientation = "vertical";
+
+  public MetricSystemEnum = MetricSystemEnum;
 
   constructor(public settingsService: SettingsService) { }
 }
