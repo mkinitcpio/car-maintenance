91917590359e5756bc877501699a7a90f2782091
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2022-12-05 22:37:04 +0300

    feat(welcome-page): add links to buttons

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { ElectronService } from '@core/services';
+import { DialogManagerService } from '@shared/services/dialog-manager.service';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss'],
 })
-export class WelcomePageComponent {}
+export class WelcomePageComponent {
+
+  private readonly GITHUB_URL = "https://github.com/mkinitcpio/car-maintenance";
+
+  constructor(
+    private electronService: ElectronService,
+    private dialogManagerService: DialogManagerService
+  ) {}
+
+  public onOpenGitHub(): void {
+    this.electronService.shell.openExternal(this.GITHUB_URL);
+  }
+
+  public onOpenMail(): void {
+    this.electronService.shell.openExternal("mailto:aliaksandr.kazhamiaka@gmail.com");
+  }
+
+  public onOpenFeedback(): void {
+    this.dialogManagerService.openFeedbackDialog();
+  }
+
+}
