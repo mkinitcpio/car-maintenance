d0a0475437dade3ac804aa5e24b0e6a3223d73b9
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2024-02-07 13:48:19 +0300

    feat(utils): extend API

--- src/app/shared/services/utils.service.ts
+++ src/app/shared/services/utils.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Record } from '../../detail/state/interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
 
-  public getResultCost(records: Record[]): number {
+  public getResultCost<T extends { cost: string }>(records: T[]): number {
     const costs = records
       .map((record) => +record.cost)
       .filter(Boolean);
 
     return costs.reduce((acc, cost) => acc + cost , 0);
+  }
+
+  public getLastDate<T extends { date: Date }>(records: T[]): Date {
+    const EMPTY = null;
+    const allDates = records
+      .filter((row) => row.date)
+      .map((row) => (new Date(row.date)).getTime());
+    const lastModifiedDate = allDates.length ? new Date(Math.max(...allDates)) : EMPTY;
+
+    return lastModifiedDate;
   }
 }
