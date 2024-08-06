49c330dfdf6a7944219257b92dbfd1ffcdcd3410
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2024-08-06 07:34:27 +0300

    feat(app): Updated components

--- src/app/shared/components/settings/setting/setting.component.ts
+++ src/app/shared/components/settings/setting/setting.component.ts
@@ -1,18 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-setting',
   templateUrl: './setting.component.html',
-  styleUrls: ['./setting.component.scss']
+  styleUrls: ['./setting.component.scss'],
 })
 export class SettingComponent {
-
-  @Input()
-  label: string;
-
-  @Input()
-  icon = '';
-
-  @Input()
-  customIcon: string;
+  public label = input<string>();
+  public icon = input<string>('');
+  public customIcon = input<string>();
 }
