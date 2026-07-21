21d971a4a5a0e7f8129a4455887b5823aa898c97
mkinitcpio <aliaksandr.kazhamiaka@gmail.com>
2026-07-21 16:15:02 +0300

    refactor(content-container): integrate typography

--- src/app/shared/components/content-container/content-container.module.ts
+++ src/app/shared/components/content-container/content-container.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentContainerComponent } from './content-container.component';
+import { CmTextHeader } from "../typography/text-header/text-header";
 
 @NgModule({
   declarations: [
     ContentContainerComponent
   ],
   imports: [
-    CommonModule
-  ],
+    CommonModule,
+    CmTextHeader,
+],
   exports: [
     ContentContainerComponent,
   ],
